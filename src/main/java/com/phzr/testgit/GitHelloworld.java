edcd6abfda6f0c25236d2221e04776c67bfdc41c
sevenDay2014 <806970740@qq.com>
2016-08-24 17:44:09 +0800

    提交代码

--- src/main/java/com/phzr/testgit/GitHelloworld.java
+++ src/main/java/com/phzr/testgit/GitHelloworld.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	//我是注释的提示代码的功能测试同步代码的功能	
 	System.out.println("哈哈哈");
 	System.out.println("我测试自己提交的代码了...");
+	System.out.println("我要在服务器上面进行写代码了，请客户端的同学进行更新操作");
 }
 
 }
