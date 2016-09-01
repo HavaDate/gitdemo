f6435fd6dc9f273eb2eff62f02c2e86f4d01c147
sevenDay2014 <806970740@qq.com>
2016-09-01 20:04:00 +0800

    zhangchudaima

--- src/main/java/com/phzr/testgit/GitHelloworld.java
+++ src/main/java/com/phzr/testgit/GitHelloworld.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 	System.out.println("哈哈哈");
 	System.out.println("我测试自己提交的代码了...");
 	System.out.println("我要在服务器上面进行写代码了，请客户端的同学进行更新操作");
+	System.out.println("我要在服务器上面进行写代码了，请客户端的同学进行更新操作11");
 }
 
 }
